ac43970055b6adb4be5e612b72f4214d35bcf5e0
Mayank-Patel1015 <141281241+Mayank-Patel1015@users.noreply.github.com>
2023-09-14 02:00:29 -0400

    Create 206.js

---
+++ 206.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+// 1->2->3->4->5 
+// ^
+// 2->1 3
+//      ^
+var reverseList = function(head) {
+    var previous = null;
+    var cur = head
+    while( cur !== null){
+        var temp = cur.next
+        cur.next = previous
+        previous = cur
+        cur = temp
+    }
+    return previous
+};
